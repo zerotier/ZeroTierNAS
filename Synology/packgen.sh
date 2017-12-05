a1df96ea84cd2a19754ee11068174435f6ab36ef
Joseph Henry <josephjah@gmail.com>
2017-12-04 16:45:05 -0800

    Added auto-build script for one-button package build process

---
+++ Synology/packgen.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Iterate over cross-compiled ZT binaries and build packages for each
+# Only builds for the version given
+
+# Run this script from the same directory as build.sh in the Synology folder
+
+CROSSDIR='/media/psf/Home/op/zt/zerotier'
+DSM_VERSION='6.1'
+ZT_VERSION='1.2.4'
+OUTPUT_DIR='/media/psf/Home/op/zt/ZeroTierNAS/Synology/FinishedPackages'
+
+mkdir -p ${OUTPUT_DIR}
+
+for dir in $(ls ${CROSSDIR})
+do
+	dir=${dir%*/}
+	if [[ $dir == *${DSM_VERSION} ]]; then
+    	#echo ${dir##*/}
+    	PACKAGE_ARCH="${dir#work-}"
+    	echo ${PACKAGE_ARCH}
+
+    	ZT_BINARY=${CROSSDIR}/$dir/ZeroTierOne-${ZT_VERSION}/zerotier-one
+    	echo ${ZT_BINARY}
+
+    	if [ -f ${ZT_BINARY} ] ; then
+    		echo "OK"
+    		# copy cross-compiled binary into package folder
+    		cp -f ${ZT_BINARY} app/bin
+    		# make the package
+    		./build.sh
+    		# remove the binary to prevent mixups
+    		rm -f ${ZT_BINARY} app/bin
+    		cp dist/zerotier-${ZT_VERSION}-noarch.spk ${OUTPUT_DIR}/zerotier-${ZT_VERSION}-syn-${PACKAGE_ARCH}.spk
+    	else
+    		echo "File does not exist. Not building."
+    	fi
+    fi
+done
